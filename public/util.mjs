c7917ee41f6889efa936c87ffc57155c72d1c27f
Avaer Kazmer <a@modules.io>
2022-10-16 22:22:35 -0400

    Update util.mjs

--- public/util.mjs
+++ public/util.mjs
@@ -1,10 +1,27 @@
+import {zbdecode} from './encoding.mjs';
+
 const alignN = n => index => {
   const r = index % n;
   return r === 0 ? index : (index + n - r);
 };
 const align4 = alignN(4);
 
+const parseUpdateObject = uint8Array => zbdecode(uint8Array);
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ return result;
+}
+const makeId = () => makeid(5);
+
 export {
   alignN,
   align4,
+  parseUpdateObject,
+  makeId,
 };
