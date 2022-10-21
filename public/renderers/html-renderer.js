3c5f7b70d03799504484ad266ffb5e282141631f
Avaer Kazmer <a@modules.io>
2022-10-21 19:04:06 -0400

    Add renderers

---
+++ public/renderers/html-renderer.js
@@ -0,0 +1,62 @@
+export class RemotePlayerHtmlRenderer {
+  constructor(remotePlayerId, localPlayerId, dataClient) {
+    this.remotePlayerId = remotePlayerId;
+    this.localPlayerId = localPlayerId;
+    this.dataClient = dataClient;
+
+    const div = document.createElement('div');
+    div.style.cssText = `\
+      position: fixed;
+      top: 0;
+      left: 0;
+      background-color: ${this.remotePlayerId === this.localPlayerId ? 'blue' : 'red'};
+      width: 10px;
+      height: 10px;
+      z-index: 100;
+      pointer-events: none;
+    `;
+    document.body.appendChild(div);
+
+    const map = this.dataClient.getArrayMap('players', this.remotePlayerId);
+    map.addEventListener('update', e => {
+      const {val} = e.data;
+      const [x, y, z] = val;
+      div.style.left = `${x}px`;
+      div.style.top = `${y}px`;
+
+      // console.log('got update', e.data);
+    });
+
+    this.cleanupFn = () => {
+      document.body.removeChild(div);
+    };
+  }
+  destroy() {
+    this.cleanupFn();
+  }
+}
+export class LocalPlayerHtmlRenderer { // XXX can be moved to controllers instead of renderers
+  constructor(realms) {
+    this.realms = realms;
+
+    const {localPlayer} = realms;
+    /* const players = this.dataClient.getArray('players');
+    const {map: player, update: playerAddUpdate} = players.addAt(this.playerId, {
+      name: this.playerId,
+      position: Float32Array.from([0, 0, 0]),
+    });
+    this.ndc.send(serializeMessage(playerAddUpdate)); */
+
+    const mousemove = e => {
+      localPlayer.setKeyValue('position', Float32Array.from([e.clientX, e.clientY, 0]));
+    };
+    window.addEventListener('mousemove', mousemove);
+
+    this.cleanupFn = () => {
+      window.removeEventListener('mousemove', mousemove);
+    };
+  }
+  destroy() {
+    this.cleanupFn();
+  }
+}
